80e03f81df45ec5c81dd5c5704fce1eabd14b60d
JH <kojh0111@naver.com>
2021-08-09 22:08:45 +0900

    WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`); // 여기서 socket은 서버로의 연결을 의미
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data); // Backend -> Frontend
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!"); // Frontend -> Backend
+}, 5000);
